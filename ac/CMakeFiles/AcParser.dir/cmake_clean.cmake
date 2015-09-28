4c58ca2aebbfdb828021eb0bdf49c58a0706758a
PangZhi <fanjing09@gmail.com>
2015-09-28 12:06:31 -0500

    code clean

--- ac/CMakeFiles/AcParser.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/AcParser"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/AcParser.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
